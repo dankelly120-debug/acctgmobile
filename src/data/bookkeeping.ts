7c7997f820a9ac015728dca512de4ec97d572bef
Dan Kelly <dk486@icc.edu>
2026-08-28 11:37:09 -0500

    fixing src folder

---
+++ src/data/bookkeeping.ts
@@ -0,0 +1,154 @@
+export const course = {
+  "code": "ICC ACCTG 105",
+  "name": "Bookkeeping",
+  "description": "A practical video sequence covering transaction analysis, the accounting cycle, receivables, payables, payroll, and financial statements.",
+  "chapters": [
+    {
+      "number": "1",
+      "title": "Introduction to Financial Accounting",
+      "items": [
+        {
+          "title": "Introduction to Financial Accounting",
+          "url": "https://youtu.be/nemNOnq0tGc",
+          "type": "video"
+        },
+        {
+          "title": "(Transactions used in next 3 videos)",
+          "url": "https://drive.google.com/file/d/1jHU1KU2jxcWSIRuf_XFFL4yF4CaIQVCT/view?usp=sharing",
+          "type": "file"
+        }
+      ]
+    },
+    {
+      "number": "2",
+      "title": "The Accounting Equation",
+      "items": [
+        {
+          "title": "Analyzing Transactions Using the Accounting Equation",
+          "url": "https://youtu.be/IUZpmTRDMTo",
+          "type": "video"
+        }
+      ]
+    },
+    {
+      "number": "3",
+      "title": "T-Accounts",
+      "items": [
+        {
+          "title": "Analyzing Transactions Using T-Accounts",
+          "url": "https://youtu.be/DkuM1j5-lOw",
+          "type": "video"
+        }
+      ]
+    },
+    {
+      "number": "4",
+      "title": "Journal Entries and the Accounting Cycle",
+      "items": [
+        {
+          "title": "Analyzing Transactions Using Journal Entries",
+          "url": "https://youtu.be/zF3jM7VJIlc",
+          "type": "video"
+        },
+        {
+          "title": "Accounting Cycle for a start-up company",
+          "url": "https://youtu.be/f70k0X4LHfo",
+          "type": "video"
+        },
+        {
+          "title": "(problem data and solution)",
+          "url": "https://drive.google.com/file/d/1QkGojxDCLIIUxqZ3c2f95yc4WVUn8edM/view?usp=sharing",
+          "type": "file"
+        }
+      ]
+    },
+    {
+      "number": "5",
+      "title": "Adjustments and the Worksheet",
+      "items": [
+        {
+          "title": "Adjustments and the Worksheet",
+          "url": "https://youtu.be/3S4KA0a_00Y",
+          "type": "video"
+        }
+      ]
+    },
+    {
+      "number": "6",
+      "title": "Closing Entries",
+      "items": [
+        {
+          "title": "Closing Entries and Post Closing TB",
+          "url": "https://youtu.be/IkGAR6GA8vQ",
+          "type": "video"
+        }
+      ]
+    },
+    {
+      "number": "7and8",
+      "title": "Doing Business on Credit",
+      "items": [
+        {
+          "title": "Doing Business on Credit",
+          "url": "https://youtu.be/Ksy0PKY-3YA",
+          "type": "video"
+        }
+      ]
+    },
+    {
+      "number": "7",
+      "title": "Accounts Receivable",
+      "items": [
+        {
+          "title": "Accounts Receivable Entries",
+          "url": "https://youtu.be/zD7iLfWwgHc",
+          "type": "video"
+        }
+      ]
+    },
+    {
+      "number": "8",
+      "title": "Accounts Payable",
+      "items": [
+        {
+          "title": "Accounts Payable Entries",
+          "url": "https://youtu.be/lTFkioq3Kr8",
+          "type": "video"
+        }
+      ]
+    },
+    {
+      "number": "10/11",
+      "title": "Payroll",
+      "items": [
+        {
+          "title": "Payroll Basics",
+          "url": "https://youtu.be/BMm0scOWC9Q",
+          "type": "video"
+        }
+      ]
+    },
+    {
+      "number": "12",
+      "title": "Financial Statement Adjustments",
+      "items": [
+        {
+          "title": "Financial Statement Adjustments",
+          "url": "https://youtu.be/oSK-lwL9_c8",
+          "type": "video"
+        }
+      ]
+    },
+    {
+      "number": "13",
+      "title": "Financial Statement Analysis",
+      "items": [
+        {
+          "title": "Financial Statement Analysis",
+          "url": "https://youtu.be/kQDC4_eaZV0",
+          "type": "video"
+        }
+      ]
+    }
+  ]
+} as const;
